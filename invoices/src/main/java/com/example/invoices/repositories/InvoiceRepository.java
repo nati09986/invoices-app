fc2a53a70ddac0efcd56338a3dd195819eee529c
netanel <nati09986@gmail.com>
2024-12-24 16:16:07 +0700

    fix bug

--- invoices/src/main/java/com/example/invoices/repositories/InvoiceRepository.java
+++ invoices/src/main/java/com/example/invoices/repositories/InvoiceRepository.java
@@ -5,25 +5,25 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface InvoiceRepository extends JpaRepository<Invoice, Long> {
-    @Query("SELECT i FROM invoices i " +
+    @Query("SELECT i FROM Invoice i " +
             "WHERE (:invoiceStatus IS NULL OR i.invoiceStatus = :invoiceStatus) " +
             "AND (:startDate IS NULL OR :endDate IS NULL OR i.invoiceDate BETWEEN :startDate AND :endDate) " +
             "AND (:companyName IS NULL OR i.supplier.companyName = :companyName)")
     List<Invoice> findInvoicesByFilters(
             @Param("invoiceStatus") String invoiceStatus,
-            @Param("startDate") LocalDate startDate,
-            @Param("endDate") LocalDate endDate,
+            @Param("startDate") LocalDateTime startDate,
+            @Param("endDate") LocalDateTime endDate,
             @Param("companyName") String companyName);
 
-    List<Invoice> findByInvoiceStatus(String invoiceStatus);
-
-    List<Invoice> findByInvoiceDateBetween(LocalDate startDate, LocalDate endDate);
-
-    List<Invoice> findByInvoiceStatusAndInvoiceDateBetween(String invoiceStatus, LocalDate startDate, LocalDate endDate);
-
-    List<Invoice> findBySupplier_CompanyName(String companyName);
+//    List<Invoice> findByInvoiceStatus(String invoiceStatus);
+//
+//    List<Invoice> findByInvoiceDateBetween(LocalDateTime startDate, LocalDateTime endDate);
+//
+//    List<Invoice> findByInvoiceStatusAndInvoiceDateBetween(String invoiceStatus, LocalDateTime startDate, LocalDateTime endDate);
+//
+//    List<Invoice> findBySupplier_CompanyName(String companyName);
 }

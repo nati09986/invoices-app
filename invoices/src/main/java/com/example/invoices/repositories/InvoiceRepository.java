acae5f6ee5ff752b9f3ec2bf1a899546f52a2c70
netanel <nati09986@gmail.com>
2024-12-25 01:12:09 +0700

    fix bug

--- invoices/src/main/java/com/example/invoices/repositories/InvoiceRepository.java
+++ invoices/src/main/java/com/example/invoices/repositories/InvoiceRepository.java
@@ -5,18 +5,18 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import java.time.LocalDateTime;
+import java.time.LocalDate;
 import java.util.List;
 
 public interface InvoiceRepository extends JpaRepository<Invoice, Long> {
     @Query("SELECT i FROM Invoice i " +
             "WHERE (:invoiceStatus IS NULL OR i.invoiceStatus = :invoiceStatus) " +
-            "AND (:startDate IS NULL OR i.invoiceDate >= :startDate) " +
-            "AND (:endDate IS NULL OR i.invoiceDate <= :endDate) " +
-            "AND (:companyName IS NULL OR i.supplier.companyName = :companyName)")
+            "AND (cast(:startDate as date) is NULL OR i.invoiceDate >= :startDate) " +
+            "AND (cast(:endDate as date) is NULL OR i.invoiceDate <= :endDate) " +
+            "AND (:contactName IS NULL OR i.supplier.contactName = :contactName)")
     List<Invoice> findInvoicesByFilters(
             @Param("invoiceStatus") String invoiceStatus,
-            @Param("startDate") LocalDateTime startDate,
-            @Param("endDate") LocalDateTime endDate,
-            @Param("companyName") String companyName);
+            @Param("startDate") LocalDate startDate,
+            @Param("endDate") LocalDate endDate,
+            @Param("contactName") String contactName);
 }

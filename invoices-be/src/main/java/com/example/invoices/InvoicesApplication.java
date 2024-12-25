d35a5b54911d104f58cee97763580c711622bddb
netanel <nati09986@gmail.com>
2024-12-26 00:13:29 +0700

    fix gradle file

--- invoices-be/src/main/java/com/example/invoices/InvoicesApplication.java
+++ invoices-be/src/main/java/com/example/invoices/InvoicesApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class InvoicesApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(InvoicesApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(InvoicesApplication.class, args);
+    }
 }

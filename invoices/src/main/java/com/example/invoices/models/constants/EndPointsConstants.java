0a42e77423c26af728887f5c0ee24384a5600e63
netanel <nati09986@gmail.com>
2024-12-25 15:30:18 +0700

    fix ui

--- invoices/src/main/java/com/example/invoices/models/constants/EndPointsConstants.java
+++
@@ -1,7 +0,0 @@
-package com.example.invoices.models.constants;
-
-public final class EndPointsConstants {
-    public static final String BASE_URL = "/api";
-
-    public static final String INVOICE_BY_ID = "/{id}";
-}
